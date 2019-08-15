12005ecc998fe48f07d0363d40cafc9b0515d254
Joakim Larsen <joakimlarsen88@gmail.com>
2019-08-15 09:31:21 +0200

    init

---
+++ Test/venv/Scripts/vba_extract.py
@@ -0,0 +1,4 @@
+#!C:\Users\JOLN\PycharmProjects\Test\venv\Scripts\python.exe
+# EASY-INSTALL-SCRIPT: 'xlsxwriter==1.1.2','vba_extract.py'
+__requires__ = 'xlsxwriter==1.1.2'
+__import__('pkg_resources').run_script('xlsxwriter==1.1.2', 'vba_extract.py')
