7f2674c8a963831e91503b63d8966469f76bad3d
Joakim Larsen <joakimlarsen88@gmail.com>
2020-02-20 13:44:58 +0100

    delete files

--- Test/tester.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from openpyxl import load_workbook
-import excelhandler
-import os
-import BBRhandler
-
-os.listdir('.')
-#excelhandler.runMacro('SBiCopy.xlsm', 'copyThings')
-BBRhandler.calculateEnergyDemand('860')
